1fa507022bc6bb5eb49e5b1f219192ef153f2428
Tom Bell <tomb@tomb.io>
2022-11-13 23:01:49 +0000

    Add LibSharedMedia register for statusbar textures

--- init.lua
+++ init.lua
@@ -9,3 +9,13 @@ L.C.tagEvents = {}
 
 -- make the config global
 oUF_RiphieConfig = L.C
+
+local LibStub = LibStub
+
+if LibStub then
+  local LSM = LibStub("LibSharedMedia-3.0")
+  LSM:Register("statusbar", "Skullflower", "Interface\\AddOns\\" .. A .. "\\media\\Skullflower")
+  LSM:Register("statusbar", "Skullflower 2", "Interface\\AddOns\\" .. A .. "\\media\\Skullflower2")
+  LSM:Register("statusbar", "Skullflower 3", "Interface\\AddOns\\" .. A .. "\\media\\Skullflower3")
+  LSM:Register("statusbar", "Skullflower Light", "Interface\\AddOns\\" .. A .. "\\media\\SkullflowerLight")
+end

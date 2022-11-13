8ee6f33700873fdab3855b4671fef4e0f0a4c4a4
Tom Bell <tomb@tomb.io>
2022-11-13 23:09:08 +0000

    Update healthbar colors

--- global.lua
+++ global.lua
@@ -13,18 +13,12 @@ L.C.backdrop = {
   tileSize = 0,
   inset = 1,
   edgeSize = 1,
-  insets = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  },
+  insets = { left = 0, right = 0, top = 0, bottom = 0 },
 }
 
 L.C.textures = {
   statusbar = L.C.mediapath .. "Skullflower2",
   statusbarBG = L.C.mediapath .. "Skullflower2",
-  absorb = L.C.mediapath .. "absorb",
   aura = L.C.mediapath .. "square",
   split = L.C.mediapath .. "split",
 }
@@ -33,19 +27,21 @@ L.C.colors = {}
 
 L.C.colors.bgMultiplier = 0.3
 
-L.C.colors.castbar = {
-  default = { 1, 0.7, 0 },
-  defaultBG = { 1 * L.C.colors.bgMultiplier, 0.7 * L.C.colors.bgMultiplier, 0 },
-  shielded = { 0.7, 0.7, 0.7 },
-  shieldedBG = { 0.7 * L.C.colors.bgMultiplier, 0.7 * L.C.colors.bgMultiplier, 0.7 * L.C.colors.bgMultiplier },
+L.C.colors.healthbar = {
+  default = { 0, 1, 0 },
+  defaultBG = { 0 * L.C.colors.bgMultiplier, 1 * L.C.colors.bgMultiplier, 0 * L.C.colors.bgMultiplier },
+
+  threat = { 1, 0, 0 },
+  threatBG = { 1 * L.C.colors.bgMultiplier, 0 * L.C.colors.bgMultiplier, 0 * L.C.colors.bgMultiplier },
+
+  threatInvers = { 0, 1, 0 },
+  threatInversBG = { 0 * L.C.colors.bgMultiplier, 1 * L.C.colors.bgMultiplier, 0 * L.C.colors.bgMultiplier },
 }
 
-L.C.colors.healthbar = {
-  -- default = { 0, 1, 0 },
-  -- defaultBG = { 0*L.C.colors.bgMultiplier, 1*L.C.colors.bgMultiplier, 0 },
-  threat = { 1, 0, 0 },
-  threatBG = { 1 * L.C.colors.bgMultiplier, 0, 0 },
-  threatInvers = { 0, 1, 0 },
-  threatInversBG = { 0, 1 * L.C.colors.bgMultiplier, 0 },
-  absorb = { 0.1, 1, 1, 0.7 },
+L.C.colors.castbar = {
+  default = { 1, 0.7, 0 },
+  defaultBG = { 1 * L.C.colors.bgMultiplier, 0.7 * L.C.colors.bgMultiplier, 0 * L.C.colors.bgMultiplier },
+
+  shielded = { 0.7, 0.7, 0.7 },
+  shieldedBG = { 0.7 * L.C.colors.bgMultiplier, 0.7 * L.C.colors.bgMultiplier, 0.7 * L.C.colors.bgMultiplier },
 }

b56a6ed955c9b4f1dd0dc34b8b5fdae57b000e66
Tom Bell <tomb@tomb.io>
2023-04-06 20:15:55 +0100

    Add truncation of name tag

--- tags.lua
+++ tags.lua
@@ -13,6 +13,8 @@ L.C.tagMethods["oUF_RiphieConfig:name"] = function(unit, ...)
   local name = UnitName(unit)
   local class = UnitClassification(unit)
 
+  name = ShortenString(name, 15, "...")
+
   if class == "elite" then
     return name .. " ++"
   elseif class == "rare" or class == "rareelite" then
@@ -22,3 +24,34 @@ L.C.tagMethods["oUF_RiphieConfig:name"] = function(unit, ...)
   end
 end
 L.C.tagEvents["oUF_RiphieConfig:name"] = "UNIT_NAME_UPDATE"
+
+function ShortenString(str, numChars, dots)
+  local bytes = #str
+  if bytes <= numChars then
+    return str
+  else
+    local len, pos = 0, 1
+    while pos <= bytes do
+      len = len + 1
+      local c = str:byte(pos)
+      if c > 0 and c <= 127 then
+        pos = pos + 1
+      elseif c >= 192 and c <= 223 then
+        pos = pos + 2
+      elseif c >= 224 and c <= 239 then
+        pos = pos + 3
+      elseif c >= 240 and c <= 247 then
+        pos = pos + 4
+      end
+      if len == numChars then
+        break
+      end
+    end
+
+    if len == numChars and pos <= bytes then
+      return strsub(str, 1, pos - 1) .. (dots and "..." or "")
+    else
+      return str
+    end
+  end
+end

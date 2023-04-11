70db9abf1775e513eec243c4860397f10adb0da1
Tom Bell <tomb@tomb.io>
2023-04-11 01:42:48 +0100

    Add nameplate show only names CVar

--- init.lua
+++ init.lua
@@ -9,3 +9,10 @@ L.C.tagEvents = {}
 
 -- make the config global
 oUF_RiphieConfig = L.C
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:SetScript("OnEvent", function()
+  SetCVar("nameplateShowOnlyNames", 1)
+  C_NamePlate.SetNamePlateFriendlySize(0.1, 0.1)
+end)

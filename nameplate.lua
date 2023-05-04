88a483308956f702cd379a271799a3edaf8de83c
Tom Bell <tomb@tomb.io>
2023-05-04 18:58:11 +0100

    Update nameplate configuration

--- nameplate.lua
+++ nameplate.lua
@@ -1,32 +1,46 @@
 local A, L = ...
 
-local cvars = {
+local NamePlateCVars = {
   nameplateMinScale = 1,
   nameplateMaxScale = 1,
+
   nameplateMinScaleDistance = 0,
   nameplateMaxScaleDistance = 40,
+
   nameplateGlobalScale = 1,
   NamePlateHorizontalScale = 1,
   NamePlateVerticalScale = 1,
   nameplateSelfScale = 1,
-  nameplateSelectedScale = 1,
+  nameplateSelectedScale = 1.1,
   nameplateLargerScale = 1,
+
   nameplateShowFriendlyNPCs = 0,
-  nameplateMinAlpha = 0.5,
-  nameplateMaxAlpha = 0.5,
+
+  nameplateMinAlpha = 0.6,
+  nameplateMaxAlpha = 0.6,
+
   nameplateMinAlphaDistance = 0,
   nameplateMaxAlphaDistance = 40,
+
   nameplateSelectedAlpha = 1,
-  nameplateOccludedAlphaMult = 0,
+  nameplateOccludedAlphaMult = 0.8,
 }
-L.C.NamePlateCVars = cvars
+L.C.NamePlateCVars = NamePlateCVars
+
+local NamePlateCustomUnits = {
+  [194649] = { 1, 0, 1 }, -- Normal Raid Dummy (Valdrakken)
+  [194644] = { 1, 0.2, 0.3 }, -- Dungeoneer's Training Dummy (Valdrakken)
+}
+L.C.NamePlateCustomUnits = NamePlateCustomUnits
 
 local function FilterAuraBuffs(element, unit, data)
   local nameplateShowSelf = data.nameplateShowSelf
   local nameplateShowAll = data.nameplateShowAll
   local isFromPlayerOrPlayerPet = data.isFromPlayerOrPlayerPet
 
-  return nameplateShowSelf or nameplateShowAll or isFromPlayerOrPet
+  -- TODO: add allow/denylist for buffs
+
+  return nameplateShowSelf or nameplateShowAll or isFromPlayerOrPlayerPet
 end
 
 local function FilterAuraDebuffs(element, unit, data)
@@ -34,36 +48,32 @@ local function FilterAuraDebuffs(element, unit, data)
   local nameplateShowAll = data.nameplateShowAll
   local isFromPlayerOrPlayerPet = data.isFromPlayerOrPlayerPet
 
+  -- TODO: add allow/denylist for buffs
+
   return nameplateShowSelf or nameplateShowAll or isFromPlayerOrPlayerPet
 end
 
 L.C.nameplate = {
   enabled = true,
-  friendly_only_name = true,
 
-  size = { 180, 25 },
+  size = { 180, 15 },
   point = { "CENTER" },
   scale = 1 * UIParent:GetScale() * L.C.globalscale,
 
   healthbar = {
-    colorThreat = true,
-    colorThreatInvers = true,
-    colorReaction = true,
-    colorClass = true,
-    colorHealth = true,
-
     frequentUpdates = true,
 
     name = {
       enabled = true,
-      size = 12,
       points = {
-        { "LEFT", 5, 1 },
-        { "RIGHT", 5, 1 },
+        { "CENTER", 0, 8 },
       },
+      font = L.C.font,
+      size = 14,
+      outline = "OUTLINE",
       align = "LEFT",
-      tag = "[name]|r",
       noshadow = true,
+      tag = "[name]|r",
     },
   },
 
@@ -74,11 +84,15 @@ L.C.nameplate = {
 
     name = {
       enabled = true,
-      size = 12,
       points = {
         { "LEFT", 2, 0 },
         { "RIGHT", -2, 0 },
       },
+      font = L.C.font,
+      size = 12,
+      outline = "OUTLINE",
+      align = "LEFT",
+      noshadow = true,
     },
 
     icon = {
@@ -89,38 +103,78 @@ L.C.nameplate = {
   },
 
   buffs = {
-    enabled = false,
-    size = 20,
-    point = { "BOTTOM", "TOP", 0, 25 },
-    num = 10,
-    cols = 4,
+    enabled = true,
+
+    size = 25,
+    point = { "BOTTOMRIGHT", "TOPRIGHT", 0, 8 },
+    num = 5,
+    cols = 5,
     spacing = 5,
-    initialAnchor = "BOTTOMLEFT",
-    growthX = "RIGHT",
+    initialAnchor = "BOTTOMRIGHT",
+    growthX = "LEFT",
     growthY = "UP",
     disableCooldown = true,
     filter = "HELPFUL|INCLUDE_NAME_PLATE_ONLY",
+
     FilterAura = FilterAuraBuffs,
+
+    duration = {
+      point = { "TOP", 0, 4 },
+      font = L.C.font,
+      size = 10,
+      outline = "OUTLINE",
+      align = "LEFT",
+      noshadow = true,
+    },
+
+    count = {
+      point = { "BOTTOMRIGHT", 2, -2 },
+      font = L.C.font,
+      size = 12,
+      outline = "OUTLINE",
+      align = "LEFT",
+      noshadow = true,
+    },
   },
 
   debuffs = {
     enabled = true,
-    size = 20,
-    point = { "BOTTOMLEFT", "TOPLEFT", 0, 5 },
+
+    size = 25,
+    point = { "BOTTOMLEFT", "TOPLEFT", 0, 8 },
     num = 5,
     cols = 5,
     spacing = 5,
     initialAnchor = "TOPLEFT",
     growthX = "RIGHT",
     growthY = "UP",
-    disableCooldown = false,
+    disableCooldown = true,
     filter = "HARMFUL|PLAYER|INCLUDE_NAME_PLATE_ONLY",
+
     FilterAura = FilterAuraDebuffs,
+
+    duration = {
+      point = { "TOP", 0, 4 },
+      font = L.C.font,
+      size = 10,
+      outline = "OUTLINE",
+      align = "LEFT",
+      noshadow = true,
+    },
+
+    count = {
+      point = { "BOTTOMRIGHT", 2, -2 },
+      font = L.C.font,
+      size = 12,
+      outline = "OUTLINE",
+      align = "LEFT",
+      noshadow = true,
+    },
   },
 
   raidicon = {
     enabled = true,
-    size = { 30, 30 },
+    size = { 28, 28 },
     point = { "RIGHT", "LEFT", -5, 0 },
   },
 }

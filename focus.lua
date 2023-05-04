0b7a09ea9b9b59d38d4a16ff91fe3925a1b5fcc3
Tom Bell <tomb@tomb.io>
2023-05-04 22:56:22 +0100

    Update focus frame configuration

--- focus.lua
+++ focus.lua
@@ -3,6 +3,7 @@ local A, L = ...
 L.C.focus = {
   enabled = true,
 
+  -- TODO: consider resize/reposition
   size = { 150, 20 },
   point = { "RIGHT", UIParent, "CENTER", -210, -308 },
   scale = 1 * L.C.globalscale,
@@ -21,45 +22,77 @@ L.C.focus = {
         { "TOPLEFT", 0, 18 },
         { "TOPRIGHT", 0, 18 },
       },
-      tag = "[oUF_RiphieConfig:name]",
+      font = L.C.font,
+      size = 12,
+      outline = "OUTLINE",
+      align = "LEFT",
       noshadow = true,
+      tag = "[oUF_RiphieConfig:name]",
     },
 
     health = {
       enabled = true,
-      size = 12,
       point = { "LEFT", 5, 0 },
-      tag = "[oUF_Riphie:health]",
+      font = L.C.font,
+      size = 12,
+      outline = "OUTLINE",
+      align = "LEFT",
       noshadow = true,
+      tag = "[oUF_Riphie:health]",
     },
 
     healthperc = {
       enabled = true,
       size = 12,
       point = { "RIGHT", -5, 0 },
-      tag = "[perhp]%",
+      font = L.C.font,
+      size = 12,
+      outline = "OUTLINE",
+      align = "LEFT",
       noshadow = true,
+      tag = "[perhp]%",
     },
   },
 
-  raidicon = {
-    enabled = false,
-    size = { 18, 18 },
-    point = { "CENTER", "LEFT", 0, 0 },
+  absorbbar = {
+    enabled = true,
+  },
+
+  powerbar = {
+    colorPower = true,
+
+    enabled = true,
+    size = { 150, 3 },
+    point = { "TOP", "BOTTOM", 0, -1 },
+
+    power = {
+      enabled = false,
+      point = { "RIGHT", -2, 0 },
+      font = L.C.font,
+      size = 12,
+      outline = "OUTLINE",
+      align = "LEFT",
+      noshadow = true,
+      tag = "[perpp]",
+    },
   },
 
   castbar = {
     enabled = true,
     size = { 150, 20 },
+    -- TODO: update Y position due to powerbar enabled
     point = { "TOP", "BOTTOM", 0, -6 },
 
     name = {
       enabled = true,
-      size = 12,
       points = {
         { "LEFT", 2, 0 },
         { "RIGHT", -2, 0 },
       },
+      font = L.C.font,
+      size = 12,
+      outline = "OUTLINE",
+      align = "LEFT",
       noshadow = true,
     },
 
@@ -68,5 +101,11 @@ L.C.focus = {
       size = { 26, 26 },
       point = { "RIGHT", "LEFT", -6, 0 },
     },
+  },
+
+  raidicon = {
+    enabled = false,
+    size = { 18, 18 },
+    point = { "CENTER", "LEFT", 0, 0 },
   },
 }

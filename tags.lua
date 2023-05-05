a78eaa150e17d3dee90cf33fddf019205cbe344c
Tom Bell <tomb@tomb.io>
2023-05-05 20:05:03 +0100

    Update name tag to be able to alias names

--- tags.lua
+++ tags.lua
@@ -1,5 +1,8 @@
 local A, L = ...
 
+local NameAliases = {}
+L.C.NameAliases = NameAliases
+
 L.C.tagMethods["oUF_RiphieConfig:status"] = function(unit, ...)
   if UnitAffectingCombat(unit) then
     return "|TInterface\\CharacterFrame\\UI-StateIcon:20:20:0:0:64:64:33:64:0:31|t"
@@ -13,7 +16,23 @@ L.C.tagMethods["oUF_RiphieConfig:name"] = function(unit, ...)
   local name = UnitName(unit)
   local class = UnitClassification(unit)
 
-  name = ShortenString(name, 15, "...")
+  if L.C.NameAliases[name] then
+    name = L.C.NameAliases[name]
+  end
+
+  return name
+end
+L.C.tagEvents["oUF_RiphieConfig:name"] = "UNIT_NAME_UPDATE"
+
+L.C.tagMethods["oUF_RiphieConfig:shortname"] = function(unit, ...)
+  local name = UnitName(unit)
+  local class = UnitClassification(unit)
+
+  if L.C.NameAliases[name] then
+    name = L.C.NameAliases[name]
+  end
+
+  name = L.F.ShortenString(name, 15, "...")
 
   if class == "elite" then
     return name .. " ++"
@@ -23,35 +42,4 @@ L.C.tagMethods["oUF_RiphieConfig:name"] = function(unit, ...)
     return name
   end
 end
-L.C.tagEvents["oUF_RiphieConfig:name"] = "UNIT_NAME_UPDATE"
-
-function ShortenString(str, numChars, dots)
-  local bytes = #str
-  if bytes <= numChars then
-    return str
-  else
-    local len, pos = 0, 1
-    while pos <= bytes do
-      len = len + 1
-      local c = str:byte(pos)
-      if c > 0 and c <= 127 then
-        pos = pos + 1
-      elseif c >= 192 and c <= 223 then
-        pos = pos + 2
-      elseif c >= 224 and c <= 239 then
-        pos = pos + 3
-      elseif c >= 240 and c <= 247 then
-        pos = pos + 4
-      end
-      if len == numChars then
-        break
-      end
-    end
-
-    if len == numChars and pos <= bytes then
-      return strsub(str, 1, pos - 1) .. (dots and "..." or "")
-    else
-      return str
-    end
-  end
-end
+L.C.tagEvents["oUF_RiphieConfig:shortname"] = "UNIT_NAME_UPDATE"

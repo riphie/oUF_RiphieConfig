e87c6ba7cedc55fe9dd7681ea8112a797a00cba7
Tom Bell <tomb@tomb.io>
2024-09-01 12:13:38 +0100

    Remove old friendly nameplate code

--- init.lua
+++ init.lua
@@ -8,11 +8,4 @@ L.C.tagMethods = {}
 L.C.tagEvents = {}
 
 -- make the config global
-oUF_RiphieConfig = L.C
-
-local frame = CreateFrame "Frame"
-frame:RegisterEvent "PLAYER_ENTERING_WORLD"
-frame:SetScript("OnEvent", function()
-  SetCVar("nameplateShowOnlyNames", 1)
-  C_NamePlate.SetNamePlateFriendlySize(0.1, 0.1)
-end)
+_G.oUF_RiphieConfig = L.C
